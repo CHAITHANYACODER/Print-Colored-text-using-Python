b1fcb81edee8ed33e8820f4195c070afd397af06
A Chaithanya Kumar <100756558+CHAITHANYACODER@users.noreply.github.com>
2022-12-03 16:13:29 +0530

    Add files via upload

---
+++ Print Coloured Text with Python.py
@@ -0,0 +1,8 @@
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is A Chaithanya Kumar "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"BTech Computer Science and Engineering")
+print(Fore.RED + Back.GREEN+ "Lovely Professional University")
